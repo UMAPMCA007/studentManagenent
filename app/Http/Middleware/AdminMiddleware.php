1c0474478038b53e89446edeef909157c3557b78
Midhun Chacko <thomas.midhun1@gmail.com>
2022-04-28 16:22:55 +0530

    miner change

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -19,7 +19,7 @@ class AdminMiddleware
         if (auth()->user()->Is_admin == 1) {
             return $next($request);
         } else {
-            return redirect('/');
+            return back();
         }
     }
 }
